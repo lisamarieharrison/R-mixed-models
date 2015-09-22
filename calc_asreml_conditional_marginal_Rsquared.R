8c0054d2bbff8b31b3eecd814353263810bc8ac2
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-09-22 13:49:35 +1000

    Changed to function format. Requires user to specify names of random effects so they can be distinguished from fixed effects in variance components table

--- calc_asreml_conditional_marginal_Rsquared.R
+++ calc_asreml_conditional_marginal_Rsquared.R
@@ -1,29 +1,36 @@
-#calculates marginal and conditional residuals for asreml mixed model object
+#calculates marginal and conditional R-squared values for asreml mixed model object
 #author: Lisa-Marie Harrison
 #date: 3/8/2015
 
-
-# Get design matrix of fixed effects from model
-Fmat <- model.matrix(eval(asreml.fit$fixed.formula)[-2], glm.spl)
-
-# Get variance of fixed effects by multiplying coefficients by design matrix
-VarF <- sum(var(as.vector(rev(asreml.fit$coefficients$fixed) %*% t(Fmat))), summary(asreml.fit)$varcomp[1:5, 2])
-
-# Get variance of random effects by extracting variance components
-VarRand <- sum(summary(asreml.fit)$varcomp[6, 2])
-
-# Get residual variance
-VarResid <- summary(asreml.fit)$varcomp[7, 2]
-
-varTotal <- VarF + VarRand + VarResid
-
-#calculate marginal R-squared
-marR2 <- VarF/varTotal
-
-#calculate conditional R-squared
-condR2 <- (VarF + VarRand)/varTotal
-
-cbind(VarF, VarRand, VarResid)
+calcRsquared <- function (obj, rand) {
+  
+  #obj: asreml model object
+  #rand: vector of strings containing names of random effects (splines are considered fixed so do not include)
+  #return: a list containing the marginal and conditional R-squared values
+  
+  #Get design matrix of fixed effects from model
+  Fmat <- model.matrix(eval(obj$fixed.formula)[-2], glm.spl)
+  
+  #Get variance of fixed effects by multiplying coefficients by design matrix
+  VarF <- sum(var(as.vector(rev(obj$coefficients$fixed) %*% t(Fmat))), summary(obj)$varcomp[grep("spl", rownames(summary(obj)$varcomp)), 2])
+  
+  #Get variance of random effects by extracting variance components
+  VarRand <- sum(summary(obj)$varcomp[grep(rand, rownames(summary(obj)$varcomp)), 2])
+  
+  #Get residual variance
+  VarResid <- summary(obj)$varcomp[grep("R!variance", rownames(summary(obj)$varcomp)), 2]
+  
+  varTotal <- VarF + VarRand + VarResid
+  
+  #calculate marginal R-squared
+  marR2 <- VarF/varTotal
+  
+  #calculate conditional R-squared
+  condR2 <- (VarF + VarRand)/varTotal
+  
+  return(list(marginal = marR2, conditional = condR2))
+  
+}
 
 
 
